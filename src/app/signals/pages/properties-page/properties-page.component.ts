356ca86b82e37862631eef5072b34ecf914d8dbb
cristhiancepeda <cristhian.cepeda@wearemo.com>
2024-05-21 09:56:42 -0500

    feat(properties-page): mutate signals

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, effect, signal } from '@angular/core';
+import { User } from '../../interfaces/signals.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-properties-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './properties-page.component.html',
-  styleUrl: './properties-page.component.scss'
+  styleUrl: './properties-page.component.scss',
 })
-export class PropertiesPageComponent {
+export class PropertiesPageComponent implements OnDestroy {
+  ngOnDestroy(): void {
+    this.userChangedEffect.destroy();
+  }
+  public user = signal<User>({
+    id: 1,
+    first_name: 'John',
+    last_name: 'Doe',
+    email: '',
+    avatar: '',
+  });
 
+  public userChangedEffect = effect(() => {
+    console.log(this.user().first_name);
+  });
+
+  public onFieldUpdated(field: keyof User, value: string) {
+    //this.user.set({ ...this.user(), [field]: value });
+
+    this.user.update((current) => {
+      switch (field) {
+        case 'email':
+          current.email = value;
+          break;
+
+        case 'avatar':
+          current.avatar = value;
+          break;
+
+        case 'first_name':
+          current.first_name = value;
+          break;
+
+        case 'last_name':
+          current.last_name = value;
+          break;
+      }
+
+      return current;
+    });
+  }
 }
